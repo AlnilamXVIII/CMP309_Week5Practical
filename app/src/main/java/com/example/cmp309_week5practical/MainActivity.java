156f34f5c47159192728cf8dec05259de1a2b7e4
AlnilamXVIII <71759866+AlnilamXVIII@users.noreply.github.com>
2021-02-23 22:58:11 +0000

    Maps functionality added
    Google MapsActivity added via an intent that links it to MainActivity. Maps API key added.

--- app/src/main/java/com/example/cmp309_week5practical/MainActivity.java
+++ app/src/main/java/com/example/cmp309_week5practical/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
@@ -27,6 +31,24 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
         //Get desired sensor from the SensorManager object via the reference
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        //Maps Event Handler
+        //Instantiate button object called mapsButton that references the mapsButton id in the layout
+        final Button mapsButton = findViewById(R.id.mapsButton);
+        //Create an onClick listener, listens for the click to call startMapsActivity()
+        mapsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Call startBrowserActivity(); after clicking the button, this is why we need the event handler
+                startMapsActivity();
+            }
+        });
+    }
+
+    private void startMapsActivity(){
+        //Create explicit intent from this activity to MapsActivity
+        Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+        startActivity(intent);
     }
 
     @Override
@@ -34,10 +56,10 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         super.onResume();
 
         //Make sure not null before registering listener
-        if(accelerometer!=null){
+        if(accelerometer!=null) {
             sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_UI);
         }
-        else{
+        else {
             AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
             builder.setMessage("No accelerometer on device").setTitle("Sensor unavailable");
 
